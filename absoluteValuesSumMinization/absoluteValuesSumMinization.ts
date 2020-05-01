050a8a2befd53fd8124ad36cb50b70e2c2178d34
Jason Welsh <jasonbwelsh@gmail.com>
2020-05-01 15:22:33 -0400

    challenge 1

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,8 @@
 function absoluteValuesSumMinimization(a: number[]): number {
-
+  return a.length % 2 !== 0 ? a[Math.floor(a.length / 2)] : a[Math.floor(a.length / 2) - 1];
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
