e564941c909d35314abff8387c86cb090fec9f81
Jason Welsh <jasonbwelsh@gmail.com>
2020-05-01 16:02:11 -0400

    challenge 3: Add Border

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,13 @@
 function addBorder(picture: string[]): string[] {
+  const wall = '*****';
+  const a = picture.map(i => `*${i}*`);
 
+  const b = ['*****', ...a, '*****']; // alternative solution : just return this
+
+  a.unshift(wall);
+  a.push(wall);
+
+  return a;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
