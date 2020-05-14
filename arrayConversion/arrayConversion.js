e9d7081cd3af56ab2a0ba322cca89983f21e905e
Jason Welsh <jasonbwelsh@gmail.com>
2020-05-14 16:02:19 -0400

    refactor arrayConversion challenge for reaadability

--- arrayConversion/arrayConversion.js
+++ arrayConversion/arrayConversion.js
@@ -1,25 +1,24 @@
 function arrayConversion(arr) {
-  let tracker = 1; // if odd add if even multiply
+  let isOdd = true; // if odd add if even multiply
   let x = arr; // array to push results to and finally return
 
   while (x.length > 1) {
-    x = arrayOp(x, tracker);
-    tracker += 1;
+    x = arrayOp(x, isOdd);
+    isOdd = !isOdd;
   }
   return x;
 }
 
-function arrayOp(arr, tracker) {
+function arrayOp(arr, isOdd) {
   const output = [];
+  let product, sum;
 
-  if (tracker % 2 === 0 && arr.length > 1) {
-    let product, sum;
+  if (!isOdd) {
     for (let i = 0; i < arr.length; i += 2) {
       product = arr[i] * arr[i + 1];
       output.push(product);
     }
-  }
-  if (tracker % 2 !== 0 && arr.length > 1) {
+  } else if (isOdd) {
     for (let i = 0; i < arr.length; i += 2) {
       sum = arr[i] + arr[i + 1];
       output.push(sum);
