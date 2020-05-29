031196c1a574b8719fd6e47873087eae6a0b0818
Jason Welsh <jasonbwelsh@gmail.com>
2020-05-29 12:39:49 -0400

    arrayReplace solution using map()

--- arrayReplace/arrayReplace.js
+++ arrayReplace/arrayReplace.js
@@ -1,15 +1,23 @@
 function arrayReplace(inputArray, elemToReplace, substitutionElem) {
-  const outputArray = [];
+  // const outputArray = [];
 
-  for (let i = 0; i < inputArray.length; i += 1) {
-    if (inputArray[i] === elemToReplace) {
-      outputArray.push(substitutionElem);
-    } else {
-      outputArray.push(inputArray[i]);
-    }
-  }
+  // for (let i = 0; i < inputArray.length; i += 1) {
+  //   if (inputArray[i] === elemToReplace) {
+  //     outputArray.push(substitutionElem);
+  //   } else {
+  //     outputArray.push(inputArray[i]);
+  //   }
+  // }
 
-  return outputArray;
+  // return outputArray;
+
+  //
+
+  const output = inputArray.map((el) =>
+    el === elemToReplace ? substitutionElem : el
+  );
+
+  return output;
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
