c3789b0e31630c77340abf531696130536257de3
Jason Welsh <jasonbwelsh@gmail.com>
2020-07-09 11:37:28 -0400

    get cells function returns array of dark cells and light cells. WIP

--- chessBoardCellColor/chessBoardCellColor.js
+++ chessBoardCellColor/chessBoardCellColor.js
@@ -2,18 +2,22 @@ function getDarkCells() {
   const xAxis = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
   const yAxis = [1, 2, 3, 4, 5, 6, 7, 8];
   const darkCells = [];
-  // const lightCells = [];
+  const lightCells = [];
   let isDarkCellEven = false;
-  xAxis.forEach((xCord) => {
+  xAxis.forEach((xCoordinate) => {
     for (let i = 0; i < yAxis.length; i += 1) {
-      const cell = `${xCord}${yAxis[i]}`;
+      const cell = `${xCoordinate}${yAxis[i]}`;
+      // handle dark cells when even
       if (isDarkCellEven && yAxis[i] % 2 === 0) darkCells.push(cell);
+      if (isDarkCellEven && yAxis[i] % 2 !== 0) lightCells.push(cell);
+      // handle dark cells odd
       if (!isDarkCellEven && yAxis[i] % 2 !== 0) darkCells.push(cell);
+      if (!isDarkCellEven && yAxis[i] % 2 === 0) lightCells.push(cell);
     }
     isDarkCellEven = !isDarkCellEven;
   });
 
-  return darkCells;
+  return [[...darkCells], [...lightCells]];
 }
 
 console.log(getDarkCells());
