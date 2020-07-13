6d8641cf7cbf7a8778a375591e0768fd038ccb93
Jason Welsh <jasonbwelsh@gmail.com>
2020-07-12 20:20:48 -0400

    commonCharacterCount challenge

--- commonCharacterCount/commonCharacterCount.js
+++ commonCharacterCount/commonCharacterCount.js
@@ -1,12 +1,32 @@
 function commonCharacterCount(s1, s2) {
-  const charMap1 = generateCharacterMap(s1);
-  const charMap2 = generateCharacterMap(s2);
+  // const charMap1 = generateCharacterMap(s1);
+  // const charMap2 = generateCharacterMap(s2);
+  //
+  const charArray1 = Object.entries(generateCharacterMap(s1));
+  const charArray2 = Object.entries(generateCharacterMap(s2));
+  let totalCommonCharacters = 0;
 
-  return [charMap1, charMap2];
+  for (let i = 0; i < charArray1.length; i += 1) {
+    for (let j = 0; j < charArray2.length; j += 1) {
+      if (charArray1[i][0] === charArray2[j][0]) {
+        if (charArray1[i][1] >= charArray2[j][1]) {
+          totalCommonCharacters += charArray2[j][1];
+        } else {
+          totalCommonCharacters += charArray1[i][1];
+        }
+      }
+    }
+  }
+  return totalCommonCharacters;
 }
 
 function generateCharacterMap(str) {
-  const stringArray = str.toLowerCase().split('');
+  let stringArray = [];
+
+  if (typeof str === 'string') {
+    stringArray = [...str.toLowerCase().split('')];
+  }
+
   const charMap = {};
 
   stringArray.forEach((char) => {
@@ -21,3 +41,6 @@ function generateCharacterMap(str) {
 }
 
 console.log(commonCharacterCount('aabcc', 'adcaa'));
+console.log(commonCharacterCount('cocoa', 'cobra'));
+console.log(commonCharacterCount('drd', 'xyz'));
+console.log(commonCharacterCount('drd', 123));
